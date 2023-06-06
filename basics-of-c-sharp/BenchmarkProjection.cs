5c7501d239a1a16c8b6834e9880a716a03a02e35
Ravindra Devrani <ravindra.devrani003@gmail.com>
2023-06-06 23:32:07 +0530

    benchmark demo

---
+++ basics-of-c-sharp/BenchmarkProjection.cs
@@ -0,0 +1,45 @@
+﻿using BenchmarkDotNet.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace basics_of_c_sharp
+{
+    [MemoryDiagnoser]
+    public class BenchmarkProjection
+    {
+        private int[] data;
+        // 1000, 10,000
+        [Params(1000,10000)]
+        public int ArraySize;
+
+        [GlobalSetup]
+        public void GlobalSetup()
+        {
+            // arraySize=1000, data= [0,1,2.....999]
+            data = Enumerable.Range(0, ArraySize).ToArray();
+        }
+
+        // [1,2,3,4] => [1*1,2*2,3*3,4*4] = [1,4,9,16]
+        [Benchmark(Baseline =true)]
+        public int[] ProjectWithForLoop()
+        {
+            var result = new int[ArraySize];
+
+            for (int i = 0; i < ArraySize; i++)
+            {
+                result[i] = data[i] * data[i];
+            }
+            return result;
+        }
+
+        [Benchmark]
+        public int[] ProjectWithLINQ()
+        {
+            return data.Select(x => x * x).ToArray();
+        }
+
+    }
+}
--- basics-of-c-sharp/Program.cs
+++ basics-of-c-sharp/Program.cs
@@ -1,12 +1,14 @@
 ﻿
+using basics_of_c_sharp;
+using BenchmarkDotNet.Running;
+
 namespace BasicsOfCSharp;
 
 public class Program
 {
-  
-    static void Main()
+   static void Main()
     {
-     
+        var summary = BenchmarkRunner.Run<BenchmarkProjection>();
     }
-
 }
+
