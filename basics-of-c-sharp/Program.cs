cb6e033a9ecdbab6968c52fe8e335d40e0ad70e6
Ravindra Devrani <ravindra.devrani003@gmail.com>
2023-12-09 16:36:32 +0530

    No of occurences of a word

--- basics-of-c-sharp/Program.cs
+++ basics-of-c-sharp/Program.cs
@@ -1,12 +1,32 @@
-﻿
-namespace BasicsOfCSharp;
+﻿using System.Diagnostics.Tracing;
+using System.Text.RegularExpressions;
 
+namespace BasicsOfCSharp;
 public class Program
 {
-  
     static void Main()
     {
-     
+        string text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. ipsum dolor lorem amet";
+        string[] words = Regex.Matches(text, @"\w+")
+                .Cast<Match>()
+                .Select(m => m.Value)
+                .ToArray();
+        Dictionary<string, int> wordCount = new();
+        foreach(var word in words)
+        {
+            string normalizedWord = word.ToLower().Trim();
+            if (wordCount.ContainsKey(normalizedWord))
+            {
+                wordCount[normalizedWord]++;
+            }
+            else
+            {
+                wordCount[normalizedWord] = 1;
+            }
+        }
+        foreach(KeyValuePair<string,int> wcp  in wordCount) 
+        {
+            Console.WriteLine($"{wcp.Key}:{wcp.Value}");
+        }
     }
-
 }
