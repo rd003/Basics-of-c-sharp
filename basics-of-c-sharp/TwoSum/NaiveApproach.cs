2798483e7d5849dcfbdff0f481a16f4cedcb498d
Ravindra Devrani <ravindra.devrani003@gmail.com>
2024-04-25 10:10:31 +0530

    two sum naive fixed

--- basics-of-c-sharp/TwoSum/NaiveApproach.cs
+++ basics-of-c-sharp/TwoSum/NaiveApproach.cs
@@ -5,27 +5,40 @@ public class NaiveApproach
     public static void ExecuteMethod()
     {
         int[] arr = [1, 15, 3, 2, 1, 14];
-        int sum = 33; //
-        (int i, int j) = GetTwoSumIndexes(arr, sum);
-        if (i >= 0 && j >= 1)
+        Console.WriteLine("array: ");
+        PrintArray(arr);
+        int[] possibleSumInputs = [15, 4, 29, 30, 9, 2, 17, 16, 22, 55, 16];
+        foreach (int sum in possibleSumInputs)
         {
-            Console.WriteLine($"indexes {i},{j}");
-        }
-        else
-        {
-            Console.WriteLine("Sum does not found");
+            (int index1, int index2) = GetTwoSumIndexes(arr, sum);
+            if (index1 >= 0 && index2 >= 1)
+                Console.WriteLine($"For sum: {sum} | indexes {index1},{index2}");
+            else
+                Console.WriteLine($"For sum: {sum} |  indexes does not found");
         }
         Console.ReadKey();
     }
-    private static (int, int) GetTwoSumIndexes(int[] arr, int sum)
+    private static (int index1, int index2) GetTwoSumIndexes(int[] arr, int sum)
     {
         for (int i = 0; i <= arr.Length - 2; i++)
         {
-            if (arr[i] + arr[i + 1] == sum)
+            for (int j = i + 1; j <= arr.Length - 1; j++)
             {
-                return (i, i + 1);
+                if (arr[i] + arr[j] == sum)
+                {
+                    return (i, j);
+                }
             }
         }
         return (-1, -1);
+    }
+
+    private static void PrintArray(int[] arr)
+    {
+        foreach (int n in arr)
+        {
+            Console.Write($"{n} ");
+        }
+        Console.WriteLine();
     }
 }
