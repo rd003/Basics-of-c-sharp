14fc3f4ea6ea804e633fa527cb7c0a22bdf23b57
Ravindra Devrani <ravindra.devrani003@gmail.com>
2024-04-27 22:52:06 +0530

    feat: fib

---
+++ basics-of-c-sharp/Fibonacci/Fibonacci.cs
@@ -0,0 +1,11 @@
+namespace BasicsOfCSharp.Fibonacci;
+
+public class Fibonacci
+{
+    public static void PrintSeries()
+    {
+
+    }
+
+    // private static void WithBasicPattern(int n=1)
+}
--- basics-of-c-sharp/Program.cs
+++ basics-of-c-sharp/Program.cs
@@ -1,3 +1,3 @@
-﻿using BasicsOfCSharp.TowSum;
+﻿using BasicsOfCSharp.Fibonacci;
 
-TwoSumOptimized.ExecuteMethod();
+Fibonacci.PrintSeries();
