f4178ce114f45cd1bd1a41a707ca05afd033c909
Ravindra Devrani <ravindra.devrani003@gmail.com>
2024-04-28 08:51:11 +0530

    feat: fibonacci with naive pattern

--- basics-of-c-sharp/Fibonacci/Fibonacci.cs
+++ basics-of-c-sharp/Fibonacci/Fibonacci.cs
@@ -4,8 +4,33 @@ public class Fibonacci
 {
     public static void PrintSeries()
     {
-
+        Console.WriteLine("How many numbers you want to print???");
+        bool valid = int.TryParse(Console.ReadLine(), out int num);
+        if (valid)
+            FibonacciBasic(num);
+        else
+            Console.WriteLine("Invalid entry");
+        Console.ReadLine();
     }
 
-    // private static void WithBasicPattern(int n=1)
+    private static int FibonacciBasic(int n = 1)
+    {
+        if (n < 3)
+        {
+            Console.WriteLine("atleast print 3 numbers");
+            return 0;
+        }
+        int n1 = 1;
+        int n2 = 1;
+        int n3;
+        Console.Write($"{n1} {n2} ");
+        for (int i = 3; i <= n; i++)
+        {
+            n3 = n1 + n2;
+            Console.Write($"{n3} ");
+            n1 = n2;
+            n2 = n3;
+        }
+        return 0;
+    }
 }
