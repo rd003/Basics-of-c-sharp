5ccadbb4c15e2e92b566406d1f8b960b6adef1d5
Ravindra Devrani <ravindra.devrani003@gmail.com>
2024-04-28 09:16:49 +0530

    patch: fib basic

--- basics-of-c-sharp/Fibonacci/Fibonacci.cs
+++ basics-of-c-sharp/Fibonacci/Fibonacci.cs
@@ -6,10 +6,12 @@ public class Fibonacci
     {
         Console.WriteLine("How many numbers you want to print???");
         bool valid = int.TryParse(Console.ReadLine(), out int num);
-        if (valid)
-            FibonacciBasic(num);
-        else
+        if (!valid)
             Console.WriteLine("Invalid entry");
+        else
+        {
+            FibonacciBasic(num);
+        }
         Console.ReadLine();
     }
 
@@ -20,17 +22,32 @@ public class Fibonacci
             Console.WriteLine("atleast print 3 numbers");
             return 0;
         }
-        int n1 = 1;
-        int n2 = 1;
-        int n3;
-        Console.Write($"{n1} {n2} ");
+        int fib = 1;
+        int prevFib = 1;
+        Console.Write($"{fib} {prevFib} ");
         for (int i = 3; i <= n; i++)
         {
-            n3 = n1 + n2;
-            Console.Write($"{n3} ");
-            n1 = n2;
-            n2 = n3;
+            int temp = fib;
+            fib += prevFib;
+            Console.Write($"{fib} ");
+            prevFib = temp;
         }
         return 0;
     }
+
+    private static int GetNthFibonacciNumber(int n = 1)
+    {
+        if (n <= 1)
+            return n;
+        int fib = 1;
+        int prevFib = 1;
+        for (int i = 2; i < n; i++)
+        {
+            int temp = fib;
+            fib += prevFib;
+            prevFib = temp;
+        }
+        return fib;
+    }
+
 }
