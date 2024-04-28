1fe0c377a23ab6f50dbb2b21c87f0686f371737b
Ravindra Devrani <ravindra.devrani003@gmail.com>
2024-04-28 09:32:11 +0530

    feat: fib iterative seperated

--- basics-of-c-sharp/Fibonacci/Fibonacci.cs
+++ basics-of-c-sharp/Fibonacci/Fibonacci.cs
@@ -10,44 +10,15 @@ public class Fibonacci
             Console.WriteLine("Invalid entry");
         else
         {
-            FibonacciBasic(num);
+            // FibonacciBasic(num);
+            for (int n = 1; n <= num; n++)
+            {
+                int fib = FibonacciIterative.GetNthFibonacciNumber(n);
+                Console.Write($"{fib} ");
+            }
+            Console.WriteLine();
         }
         Console.ReadLine();
-    }
-
-    private static int FibonacciBasic(int n = 1)
-    {
-        if (n < 3)
-        {
-            Console.WriteLine("atleast print 3 numbers");
-            return 0;
-        }
-        int fib = 1;
-        int prevFib = 1;
-        Console.Write($"{fib} {prevFib} ");
-        for (int i = 3; i <= n; i++)
-        {
-            int temp = fib;
-            fib += prevFib;
-            Console.Write($"{fib} ");
-            prevFib = temp;
-        }
-        return 0;
-    }
-
-    private static int GetNthFibonacciNumber(int n = 1)
-    {
-        if (n <= 1)
-            return n;
-        int fib = 1;
-        int prevFib = 1;
-        for (int i = 2; i < n; i++)
-        {
-            int temp = fib;
-            fib += prevFib;
-            prevFib = temp;
-        }
-        return fib;
     }
 
 }
