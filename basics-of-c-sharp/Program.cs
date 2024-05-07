5695544ba47659d69b7a5a6630e8df9dc40c4d68
Ravindra Devrani <ravindra.devrani003@gmail.com>
2024-05-07 21:28:22 +0530

    linq demo

--- basics-of-c-sharp/Program.cs
+++ basics-of-c-sharp/Program.cs
@@ -1,3 +1,16 @@
-﻿JaggedArrayDemo.MethodTwo();
+﻿// The Three Parts of a LINQ Query:
+// 1. Data source.
+int[] numbers = [0, 1, 2, 3, 4, 5, 6];
 
-Console.ReadKey();
+// 2. Query creation.
+// var numQuery = from num in numbers
+//                where num % 2 == 0
+//                select num;
+var numQuery = numbers.Where(a => a % 2 == 0);
+
+foreach (int num in numQuery)
+{
+    Console.Write("{0} ", num);
+}
+
+Console.ReadLine();
