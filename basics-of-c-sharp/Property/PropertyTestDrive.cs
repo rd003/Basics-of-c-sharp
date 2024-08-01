08ca322caa10f39b4ce6fcf557e16943a2d9d638
Ravindra Devrani <ravindra.devrani003@gmail.com>
2024-08-01 21:10:11 +0530

    feat: properties of various type

---
+++ basics-of-c-sharp/Property/PropertyTestDrive.cs
@@ -0,0 +1,32 @@
+﻿
+namespace basics_of_c_sharp.Property;
+
+//public class Person
+//{
+//    public string Name { get; set; }
+
+//    public int Age { get; set; }
+//}
+
+public abstract class Animal
+{
+    public  string Name { get; set; }
+}
+
+public class Dog : Animal
+{
+    public override string Name 
+    {
+        get
+        {
+          return ""
+        } 
+}
+
+public class PropertyTestDrive
+{
+    public static void Main()
+    {
+        
+    }
+}
